3290790b6a4c696b554ddd536b43b558bddd9009
Jaakko Lappalainen <jkk.lapp@gmail.com>
2014-01-26 20:47:32 +0100

    Now a list of JSON is accepted

--- server.py
+++ server.py
@@ -14,36 +14,37 @@ sheila = Flask(__name__)
 def insert():
 	data = request.data
 	try:
-		d = json.loads(data)
+		data = json.loads(data)
 	except ValueError:
 		logging.warning("Error inserting data")
 	# table candidate code
-	new = d.keys()
-	operation = 'Create'
-	# look for target table code
-	for key in cst.keys():
-		if subset(new,cst[key]) and len(new) <= len(cst[key]):
-			operation = 'Insert'
-			target = cst[key]
-			actual_insert(data, makeTableName(target), be)
-			break
-		if not disjoin(new,cst[key]):
-			tTag,tKeys = getSetWithMostCommonTags(new,cst)
-			if len(tTag) == 0 or len(tKeys) == 0:
-				continue
-			operation = 'Update'
-			logging.debug("\nAppending new: "+str(new)+" and existing "+str(tKeys))
-			target = list(set(new) | set(tKeys))
-			updateTable(tTag, makeTableName(target), cst, be)
-			actual_insert(data, makeTableName(target), be)
-			break
-		#subset(cst[key],new) and len(cst[key]) < len(new):
-	if operation == 'Create':
-		target = new
-		createTable(makeTableName(target), target, cst, be)
-		actual_insert(data, makeTableName(target), be)
-	
-	logging.info("Inserted: "+data+"\nResulting tables : "+str(cst)+"\nOperation: "+operation+" with target "+str(target))
+	for d in data:
+		new = d.keys()
+		operation = 'Create'
+		# look for target table code
+		for key in cst.keys():
+			if subset(new,cst[key]) and len(new) <= len(cst[key]):
+				operation = 'Insert'
+				target = cst[key]
+				actual_insert(json.dumps(d), makeTableName(target), be)
+				break
+			if not disjoin(new,cst[key]):
+				tTag,tKeys = getSetWithMostCommonTags(new,cst)
+				if len(tTag) == 0 or len(tKeys) == 0:
+					continue
+				operation = 'Update'
+				logging.debug("\nAppending new: "+str(new)+" and existing "+str(tKeys))
+				target = list(set(new) | set(tKeys))
+				updateTable(tTag, makeTableName(target), cst, be)
+				actual_insert(json.dumps(d), makeTableName(target), be)
+				break
+			#subset(cst[key],new) and len(cst[key]) < len(new):
+		if operation == 'Create':
+			target = new
+			createTable(makeTableName(target), target, cst, be)
+			actual_insert(d, makeTableName(target), be)
+		
+		logging.info("Inserted: "+json.dumps(d)+"\nResulting tables : "+str(cst)+"\nOperation: "+operation+" with target "+str(target))
 	return ""
 
 @sheila.route('/_query', methods=['POST'])
@@ -60,11 +61,14 @@ def query():
 		return "{}\n"
 	logging.debug("\nLooking for "+data+" on "+tTag)
 	r=actual_select(d, tTag, be)
-	ret='{ "results": '+json.dumps(r[0])
-	del r[0]
-	for j in r:
-		ret+=", "+json.dumps(j)
-	return ret+"}\n"
+	if len(r) > 1:
+		ret='{ "results": ['+json.dumps(r[0])
+		del r[0]
+		for j in r:
+			ret+=", "+json.dumps(j)
+		return ret+"]}\n"
+	else:
+		return '{ "results": '+r[0]+'}\n'
 
 if __name__ == '__main__':
 	confLogger()
