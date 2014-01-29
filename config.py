32119b0d578ee26429af24acdea311894d881a60
Jaakko Lappalainen <jkk.lapp@gmail.com>
2014-01-29 18:11:03 +0100

    a

--- config.py
+++ config.py
@@ -1,8 +1,42 @@
-class BEConf:
-	pass
-class IConf:
-	pass
-class SConf:
-	pass
-class LConf:
-	pass
+import ConfigParser,os
+
+class Conf:
+	_instance = None
+	def __new__(cls, *args, **kwargs):
+		if not cls._instance:
+			cls._instance = super(Conf, cls).__new__(cls, *args, **kwargs)
+		return cls._instance
+	def __repr__(self):
+		return '%r' % (self.__class__)
+	def __init__(self):
+		self.config = ConfigParser.RawConfigParser()
+		self.config.read('sheila.cfg')
+
+class BEConf(Conf):
+	def __init__(self):
+		Conf.__init__(self)
+		self.port = self.config.getint('BE', 'port')
+		self.host = self.config.get('BE', 'host')
+		self.user = self.config.get('BE', 'user')
+		self.passwd = self.config.get('BE', 'pass')
+		self.db = self.config.get('BE', 'db')
+
+class IConf(Conf):
+	def __init__(self):
+		Conf.__init__(self)
+		self.host=self.config.get('Interface', 'host')
+		self.port=self.config.getint('Interface', 'port')
+		self.debug=self.config.get('Interface', 'debug')
+
+class SConf(Conf):
+	def __init__(self):
+		Conf.__init__(self)
+		self.cstfile=self.config.get('Sheila','cstfile')
+		self.clear=self.config.getboolean('Sheila','clearOnStart')
+
+class LConf(Conf):
+	def __init__(self):
+		Conf.__init__(self)
+		self.level=self.config.get('Log','level')
+		self.maxFileSize=self.config.getint('Log','maxFileSize')
+		self.logfile=self.config.get('Log','logfile')
--- utils.py
+++ utils.py
@@ -1,22 +1,58 @@
-import hashlib,pickle,logging
-from config import SConf,LConf
+import hashlib,pickle,utils,time
+import logging
+import logging.handlers
+from shutil import copyfileobj
+import bz2,os
+import MySQLdb
+
 
 class CodeTable:
 	_instance = None
-
 	def __new__(cls, *args, **kwargs):
 		if not cls._instance:
 			cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
 		return cls._instance
 
-	def __init__(self):
+	def __init__(self,conf):
+		self.path=conf.cstfile
 		try:
-			logging.info("Loading existing CST '"+SConf.cstfile+"'...")
-			self.table = pickle.load(open(SConf.cstfile,'r'))
+			logging.info("Loading existing CST '"+conf.cstfile+"'...")
+			self.table = pickle.load(open(conf.cstfile,'r'))
 			logging.info("Existing CST loaded.")
 		except:
 			logging.warning("Loading empty CST!")
 			self.table = {}
+	def __call__(self):
+		return self.table
+	def get(self,tag):
+		try:
+			return self.table[tag]
+		except KeyError:
+			logging.critical("Accessing non existing element on CST table: "+tag)
+			raise
+	def getSet(self,tag):
+		try:
+			return set(self.table[tag])
+		except KeyError:
+			logging.critical("Accessing non existing element on CST table: "+tag)
+			raise
+	def getName(self,tag):
+		try:
+			return str(self.table[tag])
+		except KeyError:
+			logging.critical("Accessing non existing element on CST table: "+tag)
+			raise
+	def set(self,tag,value):
+		self.table[tag]=value
+	def remove(self,tag):
+		try:
+			del self.table[tag]
+		except KeyError:
+			logging.critical("Removing non existing element on CST table: "+tag)
+			raise
+	def tables(self):
+		return self.table.keys()
+
 
 
 def genCode(s): return "table_"+hashlib.md5(s).hexdigest()
@@ -33,30 +69,31 @@ def disjoin(l1,l2): return len(set(l1) & set(l2)) == 0
 def subset(l1,l2): return set(l1).issubset(set(l2))
 
 def printCST(cst):
-	print "CST state"
-	for key in cst.keys():
-		print "key: "+key+" content: "+str(cst[key])
-	print "----------------------------------------"
+	r="CST state\n"
+	for key in cst.tables():
+		r += "key: "+key+" content: "+cst.getName(key)+"\n"
+	r+= "----------------------------------------"
+	return r
 
 
 #this is for querying
 def getCommonSets(s,cst):
 	sets=[]
-	for key in cst.keys():
-		print str(cst[key])+" "+str(s)
-		if len(set(cst[key]) & set(s)) > 0:
+	for key in cst.tables():
+		if len(cst.getSet(key) & set(s)) > 0:
 			sets.append(key)
 	return sets
 
 #this one is for the best insertion
 def getSetWithMostCommonTags(s,cst):
+	logger = logging.getLogger("sheila")
 	rset=[]
 	rkey = ""
-	for key in cst.keys():
-		if len(rset) < (len(set(s) & set(cst[key]))):
-			rset = list(set(s) & set(cst[key]))
+	for key in cst.tables():
+		if len(rset) < (len(set(s) & cst.getSet(key))):
+			rset = list(set(s) & cst.getSet(key))
 			rkey = key
-			logging.debug("For set: "+str(s)+"\nmerging with "+str(cst[key])+"\nresulting in rkey "+str(rkey)+" and rset "+str(rset))
+			logger.debug("For set: "+str(s)+"\nmerging with "+cst.getName(key)+"\nresulting in rkey "+str(rkey)+" and rset "+str(rset))
 	return rkey,rset
 
 
@@ -64,13 +101,32 @@ def queryMatch(f, d1, d2):
 	if f == 'equal':
 		return d1 == d2
 
-def confLogger():
+def confLogger(conf):
 	logger = logging.getLogger("sheila")
-	logging.basicConfig(filename='sheila.log', level=logging.INFO)
-	logger.setLevel(LConf.level)
+	logger.setLevel(conf.level)
+	logging.basicConfig(filename='sheila.log')
 	# create console handler and set level to debug
+	handler = logging.handlers.RotatingFileHandler(conf.logfile, maxBytes=conf.maxFileSize, backupCount=10)
+	logger.addHandler(handler)
 	ch = logging.StreamHandler()
-	ch.setLevel(LConf.level)
-	formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s","%Y-%m-%d %H:%M:%S")
+	ch.setLevel(conf.level)
+	formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s","%Y-%m-%d %H:%M:%S")
 	ch.setFormatter(formatter)
 	logger.addHandler(ch)
+
+def clearEnvironment(sconf,beconf):
+	logger = logging.getLogger("sheila")
+	try:
+		os.remove(sconf.cstfile)
+	except OSError:
+		logger.critical("No CST file to erase")
+	try:
+		conn = MySQLdb.connect(host=beconf.host,user=beconf.user,passwd=beconf.passwd)
+		c = conn.cursor()
+		c.execute('DROP DATABASE '+beconf.db)
+		c.execute('CREATE DATABASE '+beconf.db)
+		c.close()
+		conn.close()
+	except:
+		logger.critical("Error clearing environment!")
+		raise
